e0f5436d320488d6d8e696971d67e7e7f3ba362b
H Singh <H.Singh@igglobal.com>
2024-05-10 10:43:05 +0530

    Initial commit, with updated Instructions for coding guidelines in codeReviewService.cs

---
+++ src/services/codeReviewService.ts
@@ -0,0 +1,90 @@
+import { ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate } from 'langchain/prompts'
+import { LLMChain } from 'langchain/chains'
+import { BaseChatModel } from 'langchain/dist/chat_models/base'
+import type { ChainValues } from 'langchain/dist/schema'
+import { PullRequestFile } from './pullRequestService'
+import parseDiff from 'parse-diff'
+import { LanguageDetectionService } from './languageDetectionService'
+import { exponentialBackoffWithJitter } from '../httpUtils'
+import { Effect, Context } from 'effect'
+import { NoSuchElementException, UnknownException } from 'effect/Cause'
+
+export interface CodeReviewService {
+  codeReviewFor(
+    file: PullRequestFile
+  ): Effect.Effect<ChainValues, NoSuchElementException | UnknownException, LanguageDetectionService>
+  codeReviewForChunks(
+    file: PullRequestFile
+  ): Effect.Effect<ChainValues, NoSuchElementException | UnknownException, LanguageDetectionService>
+}
+
+export const CodeReviewService = Context.GenericTag<CodeReviewService>('CodeReviewService')
+
+export class CodeReviewServiceImpl {
+  private llm: BaseChatModel
+  private chatPrompt = ChatPromptTemplate.fromPromptMessages([
+    SystemMessagePromptTemplate.fromTemplate(
+      "Act as an empathetic software engineer that's an expert in all programming languages, frameworks and software architecture."
+    ),
+    HumanMessagePromptTemplate.fromTemplate(`Your task is to review a Pull Request. You will receive a git diff.
+    Review it and suggest any improvements in code quality, maintainability, readability, performance, security, etc.
+    Identify any potential bugs or security vulnerabilities. Check it adheres to the following coding standards and best practices:
+    1.Utilize modern language features and C# versions whenever possible.
+2.Avoid obsolete or outdated language constructs.
+3.Only catch exceptions that can be properly handled; avoid catching generic exceptions.
+4.Use specific exception types to provide meaningful error messages.
+5.Use LINQ queries and methods for collection manipulation to improve code readability.
+6.Use asynchronous programming with async and await for I/O-bound operations.
+7.Be cautious of deadlocks and use Task.ConfigureAwait when appropriate.
+8.Use the language keywords for data types instead of the runtime types. For example, use string instead of System.String, or int instead of System.Int32.
+9.Use int rather than unsigned types. The use of int is common throughout C#, and it's easier to interact with other libraries when you use int. Exceptions are for documentation specific to unsigned data types.
+10.Use var only when a reader can infer the type from the expression. Readers view our samples on the docs platform. They don't have hover or tool tips that display the type of variables.
+11.Write code with clarity and simplicity in mind.
+12.Avoid overly complex and convoluted code logic.
+13.Suggest adding comments to the code only when you consider it a significant improvement.
+14.Write your reply and examples in GitHub Markdown format.
+
+The programming language in the git diff is {lang}.
+
+    git diff to review
+
+    {diff}`)
+  ])
+  private chain: LLMChain<string>
+
+  constructor(llm: BaseChatModel) {
+    this.llm = llm
+    this.chain = new LLMChain({
+      prompt: this.chatPrompt,
+      llm: this.llm
+    })
+  }
+
+  codeReviewFor = (
+    file: PullRequestFile
+  ): Effect.Effect<ChainValues, NoSuchElementException | UnknownException, LanguageDetectionService> =>
+    LanguageDetectionService.pipe(
+      Effect.flatMap(languageDetectionService => languageDetectionService.detectLanguage(file.filename)),
+      Effect.flatMap(lang =>
+        Effect.retry(
+          Effect.tryPromise(() => this.chain.call({ lang, diff: file.patch })),
+          exponentialBackoffWithJitter(3)
+        )
+      )
+    )
+
+  codeReviewForChunks(
+    file: PullRequestFile
+  ): Effect.Effect<ChainValues[], NoSuchElementException | UnknownException, LanguageDetectionService> {
+    const programmingLanguage = LanguageDetectionService.pipe(
+      Effect.flatMap(languageDetectionService => languageDetectionService.detectLanguage(file.filename))
+    )
+    const fileDiff = Effect.sync(() => parseDiff(file.patch)[0])
+
+    return Effect.all([programmingLanguage, fileDiff]).pipe(
+      Effect.flatMap(([lang, fd]) =>
+        Effect.all(fd.chunks.map(chunk => Effect.tryPromise(() => this.chain.call({ lang, diff: chunk.content }))))
+      )
+    )
+  }
+}
