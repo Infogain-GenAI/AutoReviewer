f792b8040a385a819d8893a4aa3ad2c655f00080
Harpreet Singh <150417303+h-singh1@users.noreply.github.com>
2024-05-17 11:59:43 +0530

    Delete src/controllers directory

--- src/controllers/getInputToken.ts
+++
@@ -1,8 +0,0 @@
-import { getInput } from '@actions/core'
-
-export default function getInputToken() {
-  const jiraUserEmail = getInput('JIRA_USER_EMAIL')
-  const jiraApiToken = getInput('JIRA_API_TOKEN')
-
-  return Buffer.from(`${jiraUserEmail}:${jiraApiToken}`).toString('base64url')
-}
