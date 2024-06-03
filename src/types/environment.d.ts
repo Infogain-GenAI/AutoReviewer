c761b7eb49a524bd897b0fbb13057bc8a477dd13
H Singh <H.Singh@igglobal.com>
2024-06-03 12:44:58 +0530

    Renamed env types

---
+++ src/types/environment.d.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    AZURE_OPENAI_API_KEY: string
+    AZURE_OPENAI_API_INSTANCE_NAME: string
+    AZURE_OPENAI_API_DEPLOYMENT_NAME: string
+    AZURE_OPENAI_API_VERSION: string
+  }
+}
