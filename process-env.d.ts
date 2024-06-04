735d20709a1b088c2a9617e8ef8550d6dc23c5cc
H Singh <H.Singh@igglobal.com>
2024-06-04 12:14:11 +0530

    changes to process types

--- process-env.d.ts
+++ process-env.d.ts
@@ -2,7 +2,7 @@ export {}
 
 declare global {
   namespace NodeJS {
-    interface ProcessEnv {
+    export interface ProcessEnv {
       AZURE_OPENAI_API_KEY: string
       AZURE_OPENAI_API_INSTANCE_NAME: string
       AZURE_OPENAI_API_DEPLOYMENT_NAME: string
